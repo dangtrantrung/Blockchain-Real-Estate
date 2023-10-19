ccf4833a6f1f22e555baf3d5893be3b43620bf43
dangtrantrung <trung280584@gmail.com>
2023-10-19 14:33:17 +0700

    Mocha tests transaction mint IPFS

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address public lender;
+    address public nftAddress;
+    address public inspector;
+    address payable public seller;
 
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        lender = _lender;
+        inspector = _inspector;
+        seller = _seller;
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -5,10 +5,16 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
+//Mocha tests
 describe("Escrow", () => {
   it("save the addresses", async () => {
+    //Deploy Real Estate contract
     const RealEstate = await ethers.getContractFactory("RealEstate");
     const realEstate = await RealEstate.deploy();
     console.log(realEstate.address);
+    //Mint
+    let transaction = await realEstate.mint(
+      "https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS"
+    );
   });
 });

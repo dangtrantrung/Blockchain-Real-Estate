acbc8b8a828cef81be9f4c9710b8ac7734d6f380
dangtrantrung <trung280584@gmail.com>
2023-10-21 09:56:11 +0700

    Test Escrow, Real Eastate contract & NFT conditions

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -22,4 +22,8 @@ contract Escrow {
         inspector = _inspector;
         seller = _seller;
     }
+
+    function list(uint256 _nftID) public {
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -7,14 +7,62 @@ const tokens = (n) => {
 
 //Mocha tests
 describe("Escrow", () => {
-  it("save the addresses", async () => {
+  let buyer, seller, inspector, lender;
+  let realEstate, escrow;
+  // before each test run;
+  beforeEach(async () => {
+    //setup accounts
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
+    /* console.log(signers);
+    const buyer = signers[0];
+    const seller = signers[1]; */
+
     //Deploy Real Estate contract
     const RealEstate = await ethers.getContractFactory("RealEstate");
-    const realEstate = await RealEstate.deploy();
+    realEstate = await RealEstate.deploy();
     console.log(realEstate.address);
     //Mint
-    let transaction = await realEstate.mint(
-      "https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS"
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS"
+      );
+    await transaction.wait();
+
+    //Deploy Escrow contract
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
     );
+  });
+
+  //test chai mocha conditions
+  describe("Deployment", () => {
+    it("Return NFT address", async () => {
+      const result = await escrow.nftAddress();
+      expect(result).to.be.equal(realEstate.address);
+    });
+    it("Return seller address", async () => {
+      const result = await escrow.seller();
+      expect(result).to.be.equal(seller.address);
+    });
+
+    it("Return inspector address", async () => {
+      const result = await escrow.inspector();
+      expect(result).to.be.equal(inspector.address);
+    });
+    /*  it("Return buyer address", async () => {
+      const result = await escrow.buyer();
+      expect(result).to.be.equal(buyer.address);
+    }); */
+    it("return lender address", async () => {
+      const result = await escrow.lender();
+      expect(result).to.be.equal(lender.address);
+    });
+
+    /* it("get signers & save the addresses", async () => {}); */
   });
 });
